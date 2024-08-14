e3a61b55cc338c061faaa2a20763a23eb7432f67
jin-hatanaka <ringo.lemon15@gmail.com>
2024-08-14 16:16:28 +0900

    HC課題(ruby グループ分け問題)の修正 #1 条件分岐を不要にするよう修正。

--- ruby/random.rb
+++ ruby/random.rb
@@ -3,20 +3,19 @@ members = ['A', 'B', 'C', 'D', 'E', 'F']
 # 実行ごとに結果が変わるように６人のリストをシャッフル
 shuffule_members = members.shuffle
 
-# 実行ごとにif文の選択が変わるように配列の要素を1個ランダムに選ぶ
-select = [1, 2].sample
-
-# 3人と3人、または2人と4人にグループを分ける
 # 2人と4人にグループを分ける
-if select == 1
-  group1 = shuffule_members.slice(0..1)
-  group2 = shuffule_members.slice(2..5)
+devide1 = shuffule_members.slice(0..1)
+devide2 = shuffule_members.slice(2..5)
+group1 = [devide1, devide2]
+
 # 3人と3人にグループを分ける
-elsif select == 2
-  group1 = shuffule_members.slice(0..2)
-  group2 = shuffule_members.slice(3..5)
-end
+devide3 = shuffule_members.slice(0..2)
+devide4 = shuffule_members.slice(3..5)
+group2 =[devide3, devide4]
+
+# 配列の要素を1個ランダムに選ぶ
+select = [group1, group2].sample
 
 # アルファベット順にして出力
-p group1.sort
-p group2.sort
+p select[0].sort
+p select[1].sort
