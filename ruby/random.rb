e5bc5f62f454376975c813d8ddc64f77683add0c
jin-hatanaka <ringo.lemon15@gmail.com>
2024-08-15 10:43:17 +0900

    HC課題(ruby グループ分け問題)の修正 #2

--- ruby/random.rb
+++ ruby/random.rb
@@ -3,19 +3,13 @@ members = ['A', 'B', 'C', 'D', 'E', 'F']
 # 実行ごとに結果が変わるように６人のリストをシャッフル
 shuffule_members = members.shuffle
 
-# 2人と4人にグループを分ける
-devide1 = shuffule_members.slice(0..1)
-devide2 = shuffule_members.slice(2..5)
-group1 = [devide1, devide2]
-
-# 3人と3人にグループを分ける
-devide3 = shuffule_members.slice(0..2)
-devide4 = shuffule_members.slice(3..5)
-group2 =[devide3, devide4]
-
 # 配列の要素を1個ランダムに選ぶ
-select = [group1, group2].sample
+x = [1, 2].sample
+
+# 3人と3人、または2人と4人にグループを分ける
+devide1 = shuffule_members.slice(0..x)
+devide2 = shuffule_members.slice(x+1..5)
 
 # アルファベット順にして出力
-p select[0].sort
-p select[1].sort
+p devide1.sort
+p devide2.sort
